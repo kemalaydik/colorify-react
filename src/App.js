466929725e1c431a4c56419adaacb05d7c0b38e4
Kemal Aydık <kemalaydik@gmail.com>
2022-07-30 12:07:30 +0300

    complete rndColors

--- src/App.js
+++ src/App.js
@@ -1,37 +1,57 @@
 import React, { Component } from 'react';
-import Coin from './Coin';
 
 export default class App extends Component {
+	static defaultProps = {
+		colorsArr: [
+			'bg-zinc-500',
+			'bg-neutral-500',
+			'bg-stone-500',
+			'bg-red -50',
+			'bg-orange-500',
+			'bg-amber-500',
+			'bg-yellow-500',
+			'bg-lime-500',
+			'bg-green-500',
+			'bg-emerald-500',
+			'bg-teal-500',
+			'bg-cyan-500',
+			'bg-sky -50',
+			'bg-blue-500',
+			'bg-indigo-500',
+			'bg-violet-500',
+			'bg-purple-500',
+			'bg-fuchsia-500',
+			'bg-pink-500',
+			'bg-rose-500'
+		]
+	};
+
 	constructor(props) {
 		super(props);
 		this.state = {
-			numTails: 0,
-			numFlips: 0,
-			numHeads: 0,
-			rnd: 0
+			bgColor: Array.from({ length: 20 }, (e, i) => {
+				return { [i]: this.genRndColor() };
+			})
 		};
-		this.flip = this.flip.bind(this);
+		this.handleClick = this.handleClick.bind(this);
+		this.genRndColor = this.genRndColor.bind(this);
 	}
-	flip() {
-		const rnd = ~~(Math.random() * 2);
-		this.setState(st => ({
-			numFlips: st.numFlips + 1,
-			[rnd ? 'numTails' : 'numHeads']: rnd ? st.numTails + 1 : st.numHeads + 1,
-			rnd
-		}));
+
+	genRndColor() {
+		return this.props.colorsArr[~~(Math.random() * 20)];
 	}
+
+	handleClick(e) {
+		const idx = e.target.getAttribute('idx');
+		this.setState(st => (st.bgColor[idx] = { [idx]: this.genRndColor() }));
+	}
+
 	render() {
 		return (
-			<div className='container flex flex-col items-center justify-center gap-2 mx-auto my-3'>
-				<h1>Lets flip a coin</h1>
-				<Coin side={this.state.rnd} />
-				<button
-					onClick={this.flip}
-					type='button'
-					class='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>
-					Flip me!!
-				</button>
-				<p>{`flips:${this.state.numFlips}, heads:${this.state.numHeads}, tails:${this.state.numTails}`}</p>
+			<div className='grid grid-cols-5'>
+				{this.state.bgColor.map((e, i) => (
+					<div onClick={this.handleClick} className={`w-full h-24 ${e[i]}`} key={i} idx={i}></div>
+				))}
 			</div>
 		);
 	}
