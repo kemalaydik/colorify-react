48220ae9fd7a00cb6579060a3036e6ab44d37328
Kemal Aydık <kemalaydik@gmail.com>
2022-09-01 14:24:40 +0300

    reset

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
-import { Routes, Route, useParams, useLocation } from 'react-router-dom';
-import Dogs from './Dogs';
-import Nav from './nav';
-import Home from './home';
 const App = () => {
 	const location = useLocation();
 	console.log(location);
-	return (
-		<div className='min-h-screen bg-gradient-to-br from-gray-200 via-orange-100 to-gray-300'>
-			<Nav />
-			<Routes>
-				<Route path='/'>
-					<Route index element={<Home />} />
-					<Route path=':dogs' element={<Dogs />} />
-				</Route>
-			</Routes>
-		</div>
-	);
+	return <></>;
 };
 
 export default App;
--- src/Dogs.js
+++
@@ -1,7 +0,0 @@
-import { useParams } from 'react-router-dom';
-const Dogs = props => {
-	const { dogs } = useParams();
-	return <>{dogs}</>;
-};
-
-export default Dogs;
