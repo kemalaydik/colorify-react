2a3c6935ddbcc40880ace7c47e17618131fc6911
Kemal AYDIK <kemalaydik@Kemals-MacBook-Pro.local>
2022-09-25 13:24:40 +0300

    add color overlay

--- src/Palette.js
+++ src/Palette.js
@@ -3,12 +3,12 @@ import ColorCard from './ColorCard';
 
 export default ({ paletteName, id, emoji, colors }) => (
 	<div className='flex flex-col min-h-screen'>
-		<header className='h-[10vh]'>asda</header>
+		<header className='h-[8vh]'>asda</header>
 		<div className='grid grid-cols-1 md:grid-cols-4 grow'>
 			{colors.map(color => (
 				<ColorCard key={color.name} {...color} />
 			))}
 		</div>
-		<footer className='h-[10vh]'>bbb</footer>
+		<footer className='h-[8vh]'>bbb</footer>
 	</div>
 );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
 	content: ['./src/*.{js,jsx}', 'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {}
+		extend: {
+			animation: {
+				grow: 'grow .5s ease-in-out 1s'
+			}
+		},
+		keyframes: {
+			grow: {
+				'0%': { transform: 'scale(1) translateY(-50%)', opacity: 0 },
+				'100%': { transform: 'scale(2) translateY(-50%)', opacity: 1 }
+			}
+		}
 	},
 	plugins: [require('flowbite/plugin')]
 };
