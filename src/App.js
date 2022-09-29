9362b7e62a3d3e6d52c742539053a8d642ecebbe
Kemal AYDIK <kemalaydik@Kemals-MacBook-Pro.local>
2022-09-29 11:04:13 +0300

    add UserContext

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import Palette from './Palette';
-import React from 'react';
+import { createContext } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
+import seedColors from './seedColors';
 
 const Home = () => {
 	return (
-		<h1>
+		<nav>
 			<Link to={'palette/material-ui-colors'}>Material UI Colors</Link>
-		</h1>
+			<Link to={'palette/flat-ui-colors-v1'}>Flat UI Colors</Link>
+			<Link to={'palette/flat-ui-colors-dutch'}>Flat UI Colors Dutch</Link>
+		</nav>
 	);
 };
 
 const App = () => {
+	const UserContext = createContext();
 	return (
 		<>
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='palette/:id' element={<Palette />} />
-			</Routes>
+			<UserContext.Provider value={seedColors}>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='palette/:id' element={<Palette />} />
+				</Routes>
+			</UserContext.Provider>
 		</>
 	);
 };
-
-export default App;
--- src/Palette.js
+++ src/Palette.js
@@ -1,16 +1,19 @@
 /* eslint-disable import/no-anonymous-default-export */
 import ColorCard from './ColorCard';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import Slider from '@mui/material/Slider';
 import LightModeSharpIcon from '@mui/icons-material/LightModeSharp';
 import NightlightIcon from '@mui/icons-material/Nightlight';
 import SelectColor from './SelectColor';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useParams } from 'react-router-dom';
-import seedColors from './seedColors';
+// import seedColors from './seedColors';
 import generatePalette from './colorHelpers';
+import PaletteIcon from '@mui/icons-material/Palette';
+import { Link } from 'react-router-dom';
 
 export default () => {
+	const seedColors = useContext(UserContext);
 	const findId = id => seedColors.find(e => e.id === id);
 	const { id } = useParams();
 	const { paletteName, emoji, shades } = generatePalette(findId(id));
@@ -36,6 +39,9 @@ export default () => {
 	return (
 		<div className='flex flex-col min-h-screen'>
 			<header className='flex items-center h-24 gap-5 pt-16 pb-12 bg-gray-300 md:px-16'>
+				<Link to='/'>
+					<PaletteIcon fontSize='large' />
+				</Link>
 				<LightModeSharpIcon color='warning' fontSize='large' />
 				<Slider aria-label='shades' defaultValue={500} valueLabelDisplay='on' step={100} marks={matches ? marks : true} min={100} max={900} onChange={el => setLevel(el.target.value)} />
 				<NightlightIcon fontSize='large' />
